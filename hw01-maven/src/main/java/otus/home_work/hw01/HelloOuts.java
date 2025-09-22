eac1a27140e0bb580064046012c6992fecbab623
EvgenySaltykov <rabota19801@ramler.ru>
2025-09-22 15:18:52 +0300

    change 'tessts' on the 'maven'

--- hw01-maven/src/main/java/otus/home_work/hw01/HelloOuts.java
+++ hw01-maven/src/main/java/otus/home_work/hw01/HelloOuts.java
@@ -6,6 +6,6 @@ public class HelloOuts {
     public static void main(String[] args) {
         System.out.println("Test");
         Joiner joiner = Joiner.on("_").useForNull("Otus");
-        System.out.printf(joiner.join("Test", "Home", "Work", "tessts", null));
+        System.out.printf(joiner.join("Test", "Home", "Work", "maven", null));
     }
 }
